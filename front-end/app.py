8a276ecb8336290b95c709824496fe3eee3344c6
Murilo Dionizio <murilomelodionizio@gmail.com>
2025-10-21 14:20:39 -0300

    iniciando front-end com api

---
+++ front-end/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import requests
+
+API_URL = "http://127.0.0.1:8000"
+st.set_page_config(page_title="Gerenciador de produtos", page_icon="📦🧾")
+st.title("📦 Estoque de produtos ")
+
